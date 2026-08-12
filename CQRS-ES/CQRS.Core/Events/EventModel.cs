4a1b9da5ea47c371fbce3c40b87ff0dff939021b
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2026-08-12 23:27:31 +0300

    add EventModel for Mongo

---
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CQRS.Core.Events;
+
+public class EventModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public Guid Id { get; set; }
+
+    public DateTime TimeStamp { get; set; }
+    public Guid AggregateIdentifier { get; set; }
+    public string AggregateType { get; set; }
+    public int Version { get; set; } // Version of the Aggregate at that time
+    public string EventType { get; set; }
+    public BaseEvent EventData { get; set; } // We have all of the event information, for replaying the event store
+}
