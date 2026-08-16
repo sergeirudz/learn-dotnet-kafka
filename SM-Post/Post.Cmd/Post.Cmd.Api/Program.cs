0a59d5f7edf3815046f7f4dbb657a933d7e28dd8
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2026-08-16 21:22:48 +0300

    Register all Post command handlers

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -4,6 +4,7 @@ using CQRS.Core.infrastructure;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
@@ -17,8 +18,24 @@ builder.Services.AddOpenApi();
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>(); // Depends on MongoDB
 builder.Services.AddScoped<IEventStore, EventStore>(); // Depends on IEventStoreRepository
-builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>,EventSourcingHandler>(); // Depends on IEventStore
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>(); // Depends on IEventStore
 builder.Services.AddScoped<ICommandHandler, CommandHandler>(); // Depends on IEventSourcingHandler
+
+// Register all command handler methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+
+// Mediator register commands
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+// Register once 
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher); // has all the commands registered
+
 
 var app = builder.Build();
 
